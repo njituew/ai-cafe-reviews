6faab1f124fc946ea4b6392352f5ae6e2280a37d
van-create <ivdmeliseev@edu.hse.ru>
2025-03-21 02:06:55 +0300

    fix ssl problem & merge ai functionality files

--- src/ai_utils.py
+++ src/ai_utils.py
@@ -5,12 +5,20 @@ import io
 import asyncio
 
 from tempfile import NamedTemporaryFile
+from transformers import pipeline
 
+process = pipeline("text-classification", model="tabularisai/multilingual-sentiment-analysis")
+
+async def get_tonality(text: str) -> str:
+    result = process(text)
+
+    return result[0]["label"]
+
+
+ssl._create_default_https_context = ssl._create_unverified_context
 model = whisper.load_model("medium")
 
 async def speech_to_text(audio_buffer: io.BytesIO) -> str:
-    ssl._create_default_https_context = ssl._create_unverified_context
-
     with NamedTemporaryFile(delete=False, suffix=".oga") as temp_file:
         temp_file.write(audio_buffer.read())
         temp_file_path = temp_file.name
--- src/review_processing.py
+++
@@ -1,9 +0,0 @@
-from transformers import pipeline
-
-process = pipeline("text-classification", model="tabularisai/multilingual-sentiment-analysis", device="mps")
-
-async def get_tonality(text: str) -> str:
-    result = process(text)
-
-    return result[0]["label"]
-
