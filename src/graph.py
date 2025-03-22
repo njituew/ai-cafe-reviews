43938b1e169cedd4b89a9d36a61d26711ad1d2af
CrazyCucumber1337 <isdybnova@edu.hse.ru>
2025-03-23 01:21:40 +0300

    Full calculation of the grade distribution

--- src/graph.py
+++ src/graph.py
@@ -4,19 +4,23 @@ matplotlib.use('Agg')  # неинтерактивный бэкенд
 
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+from db.utils import get_reviews_by_time
 from io import BytesIO
-
 from datetime import date, timedelta
 
 
-async def distribution_of_ratings(list_of_ratings: list[int]) -> BytesIO:
+async def distribution_of_ratings() -> BytesIO:
     """
     Показывает сколько отзывов имеет конкретную оценку
 
     Args:
         list_of_ratings (list[int]): список, где индекс - это оценка (-1), значение - количество отзывов с такой оценкой.
     """
-    x = [i + 1 for i in range(len(list_of_ratings))]
+    list_of_ratings = [0] * 5
+    list_of_reviews = await get_reviews_by_time(date(1, 1, 1), date.today())
+    for review in list_of_reviews:
+        list_of_ratings[review.rating - 1] += 1
+    x = [i + 1 for i in range(5)]
     y = list_of_ratings
 
     sns.set_style("darkgrid")
@@ -24,6 +28,7 @@ async def distribution_of_ratings(list_of_ratings: list[int]) -> BytesIO:
     plt.figure(figsize=(6, 4))
     sns.barplot(x=x, y=y, hue=x, legend=False, palette="crest")
 
+    plt.ylim(bottom=0)
     plt.title("График распределения оценок")
     plt.xlabel("Оценка")
     plt.ylabel("Количество отзывов")
@@ -35,7 +40,8 @@ async def distribution_of_ratings(list_of_ratings: list[int]) -> BytesIO:
 
     return buffer
 
-async def changes_of_average_grade(list_of_grades: list[float]) -> BytesIO:
+
+async def dynamics_of_satisfaction(list_of_grades: list[float]) -> BytesIO:
     """
     Показывает как изменялась средняя оценка со временем.
     Период расчётов - последние k дней, где k - длина списка.
@@ -68,5 +74,3 @@ async def changes_of_average_grade(list_of_grades: list[float]) -> BytesIO:
     plt.close()
 
     return buffer
-
-# UwU
