30d58a04e114ab8325cfbd79f8dac5c7c9c82ee5
CrazyCucumber1337 <isdybnova@edu.hse.ru>
2025-03-22 19:43:40 +0300

    Creating a graph with the distribution of ratings

--- src/graph.py
+++ src/graph.py
@@ -1,3 +1,4 @@
+import seaborn as sns
 import matplotlib
 matplotlib.use('Agg')  # неинтерактивный бэкенд
 
@@ -5,20 +6,28 @@ import matplotlib.pyplot as plt
 from io import BytesIO
 
 
-async def test_graph() -> BytesIO:
-    x = [1, 2, 3, 4, 5]
-    y = [10, 20, 25, 30, 40]
-    plt.figure(figsize=(6, 4))
-    plt.plot(x, y, marker='o')
-    plt.title("test")
-    plt.xlabel("X")
-    plt.ylabel("Y")
+async def distribution_of_ratings(list_of_ratings: list[int]) -> BytesIO:
+    """
+    Показывает сколько отзывов имеет конкретную оценку
 
-    # тут мы сохраняем график в буфер (чтобы не делать файлик)
+    Args:
+        list_of_ratings (list[int]): список, где индекс - это оценка (-1), значение - количество отзывов с такой оценкой.
+    """
+    x = [i + 1 for i in range(len(list_of_ratings))]
+    y = list_of_ratings
+
+    sns.set_style("darkgrid")
+
+    plt.figure(figsize=(6, 4))
+    sns.barplot(x=x, y=y, hue=x, legend=False, palette="crest")
+
+    plt.title("График распределения оценок")
+    plt.xlabel("Оценка")
+    plt.ylabel("Количество отзывов")
+
     buffer = BytesIO()
     plt.savefig(buffer, format='png', dpi=100)
     buffer.seek(0)  # сбрасываем указатель в 0 чтобы потом прочитать файл с начала
     plt.close()
-    
-    # возвращаем байтики в буфере
+
     return buffer
